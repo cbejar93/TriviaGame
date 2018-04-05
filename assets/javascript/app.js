048730c03e2f87b76396d7bbc504c77e0f9e3f01
unknown <cbejar93@gmail.com>
2018-04-05 08:30:45 -0400

    got the timer to display end game once it hits 0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,17 +8,23 @@ var trivia = [
     {questions:"What is the capital of the Central African Republic?",
     answers:["Lagos","Braazerville", "Bangui", "Sibut"], correct: "Bangui"},
     
-   {questions: "What is the capital of Kosovo?", answers:[ "Belgrade", "Saraveijo","Prisine", "Zagreb"], correct: "Prisine"}
+   {questions: "What is the capital of Kosovo?", answers:[ "Belgrade", "Saraveijo","Prisine", "Zagreb"], correct: "Prisine"},
+
+   {questions: "What is the capital of Moldova?", answers:["Kiev", "Dontesk", "Chisinau", "Stavastapool"], correct:"Chisinau"},
+
+   {questions: "What is the capital of Canada?", answers:["Toronto", "Ottowa", "Washington DC", "Edmonton"], correct: "Ottowa"}
 ]; 
 // This is the countdown clock
 let time= 30;
 
 var intervaleId;
-   
-   
-console.log(trivia.length);
-
-console.log(trivia[0].questions);
+// This is to count the number of correct inputs
+var correctA;
+// This is to display the number of incorrect inputs
+var incorrectA;
+ 
+// This is where the users button presses values goes
+var guess =[];
 
 function timer(){
     intervaleId = setInterval(count, 1000);
@@ -30,8 +36,10 @@ function count(){
     time --;
     document.getElementById("timer").innerHTML= time;
     if(time===0){
-        stop()
-    }
+        stop();
+        endscreen();
+        check();
+    };
 };
 
 function stop(){
@@ -54,13 +62,12 @@ function hideShow(){
 };
 
 
-
-
 // This is a for loop for displaying the question and the answer in new elements while at the same time getting a value for the button
 function qaLoop(){
-    for (var i = 0; i < 3; i++){
-        $("#quiz").append("<p>" + trivia[i].questions + "</p> <br>");
+    for (var i = 0; i < 5; i++){
+        $("#quiz").append("<p class='portal'>" + trivia[i].questions + "</p> <br>");
      
+        // This for loop is to display the possible answers
         for (var j = 0; j< 4 ; j++){
     
             $("#quiz").append("<input type ='radio' name='question'"+i+" value="+trivia[i].answers[j]+"><span>"+trivia[i].answers[j]+"</span");
@@ -70,8 +77,7 @@ function qaLoop(){
     };
 };
 
-
-
+// This is my overall function where I put the functions to hide certain elements to produce the Qs and As
 function quizlayOut(){
 
     hideShow();
@@ -79,38 +85,63 @@ function quizlayOut(){
     qaLoop();
 
     timer();
-
-    
-
-    
-    
+      
 };
-var guess =[];
 
+
+// The catalyst for the entire quiz events
+$("#start").click(quizlayOut);
+
+// This is to read the users button presses
 $("#quiz").on("click", 'input', function(){
-           
-    
     
     guess.push(this.value);
     console.log(guess);
     
 });
 
-$("#start").click(quizlayOut);
+function endscreen(){
+     // This is to show the final card where you can restart and the score 
+     document.getElementById("card1").style.display = "block";
+    
+     // This is to hide all the remaining element in the page.
+     document.getElementById("quiz").style.display = "none";
+     document.getElementById("enter").style.display = "none";
+     document.getElementById("timer").style.display = "none";
+}
+
 
 
 // This is where all other elements are hidden and the user is able to go back to start area.
-$("#enter").on("click", function(){
-    // This is to show the final card where you can restart and the score 
-    document.getElementById("card1").style.display = "block";
+$("#enter").on("click", function ender(){
+    endscreen();
+    check();
+
+});
+
+// This is supposed to check the answers 
+function check(){
+guess.forEach(function(index){
     
-    // This is to head all the remaining element in the page.
-    document.getElementById("quiz").style.display = "none";
-    document.getElementById("enter").style.display = "none";
-    document.getElementById("timer").style.display = "none";
-
-   
+      if (index === trivia[index].correct){
+          correctA++;
+          $("#ca").append.correctA;
+          console.log(correctA);
+      }
+      else{
+          incorrectA++;
+          $("#ica").append.incorrectA;
+          console.log(incorrectA);
+      }
+    
+  });
+}
 
 });
 
-});
+// This is the function to check the ansers
+// function check() {
+//     if ($("input[name=option]:checked").val() == all[currentQ].correct) {
+//         correctA++;
+//     }
+// };
